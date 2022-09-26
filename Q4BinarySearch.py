25162d70b269f4b8376c91d501163f05b6a2c683
Di <altusj@gmail.com>
2022-09-26 11:46:30 -0400

    Binary search solution

---
+++ Q4BinarySearch.py
@@ -0,0 +1,16 @@
+def binary_itr(arr, start, end, target):
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == target: return mid
+        elif target > arr[mid]:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return -1
+
+
+arr = [2, 5, 6, 7, 8, 11, 12, 44, 55]
+target = 55
+
+result = binary_itr(arr, 0, len(arr) - 1, target)
+print(result)
